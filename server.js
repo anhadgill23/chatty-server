b883f75c5ef7fe351b6ac60de6780b1a1f8b4e44
Anhad Gill <gillanhad23@gmail.com>
2018-04-20 00:40:10 +0000

    Broadcasts the number of users

--- server.js
+++ server.js
@@ -21,17 +21,7 @@ const wss = new SocketServer({ server });
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
-
-    ws.on('message', function incomingMessage(event) {
-        let parsedData = JSON.parse(event);
-        if (parsedData){
-            parsedData.id = uuidv1();
-            wss.broadcast(JSON.stringify(parsedData));
-            console.log("Data broadcasted");
-        } else {
-            console.log('There is no data to be parsed')
-        }
-    });
+    //console.log(`The number of users are ${wss.clients.size}.`);
 
     wss.broadcast = function broadcast(data) {
         wss.clients.forEach(function each (client) {
@@ -41,6 +31,48 @@ wss.on('connection', (ws) => {
         })
     }
 
+    const broadcastUserCount = () => {
+        let userCount = {
+            type: 'userCount',
+            numOfUsers: wss.clients.size
+        }
+        console.log('userCount: ', userCount);
+        wss.broadcast(JSON.stringify(userCount));
+    }
+    broadcastUserCount();
+
+    ws.on('message', function incomingMessage(event) {
+        //USE wss.clients.size TO COUNT THE NUMBER OF CLIENTS THAT ARE CONNECTED
+        const ID = uuidv1();
+        let parsedData = JSON.parse(event);
+        if (parsedData){
+
+            if (parsedData.type === 'postMessage') {
+                parsedData.type = 'incomingMessage'
+                parsedData.id = ID;
+                wss.broadcast(JSON.stringify(parsedData));
+                console.log("Data broadcasted");
+            }
+            else if (parsedData.type === 'postNotification') {
+                parsedData.type = 'incomingNotification'
+                parsedData.id = ID;
+                wss.broadcast(JSON.stringify(parsedData));
+                console.log("Data broadcasted");
+            }
+            else {
+                console.log('Data type is neither postMessage nor postNotification');
+            }
+        } else {
+            console.log('There is no data to be parsed')
+        }
+
+    });
+
+
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+      broadcastUserCount();
+      console.log('Client disconnected')
+    }
+  );
 });
